a572fe2ca5c13f7d4b964144ce91826546e6fed2
Michal Ogorek <michal.ogorek03@gmail.com>
2025-10-29 12:49:56 +0100

    zbudowano footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -3,10 +3,45 @@ const Footer = () =>{
 
     return (
         <footer className="footer__container">
-            uwaga footer
+            <div className="footer_inner">
+                    <div className="footer__info">
+                        <div className="footer__item">
+                            <h3>O nas:</h3>
+                            <ul className="footer__list">
+                                <li>Multiscreen: 7 sal z Dolby Atmos</li>
+                                <li>Kawiarnia i strefa gier</li>
+                                <li>Program lojalnościowy „Last Club”</li>
+                                <li>Seanse specjalne: maratony, przedpremiery</li>
+                                <li>Seanse przyjazne sensorycznie</li>
+                            </ul>
+                        </div>
+                        <div className="footer__item">
+                            <h3>Informacje:</h3>
+                            <ul className="footer__list">
+                                <li>Repertuar i bilety online</li>
+                                <li>Cennik i zniżki (student/senior)</li>
+                                <li>Regulamin kina</li>
+                                <li>Godziny otwarcia i dojazd</li>
+                                <li>Kontakt i FAQ</li>
+                            </ul>
+                        </div>
+                        <div className="footer__item">
+                            <h3>Social Media:</h3>
+                            <ul className="footer__list">
+                                <li>Facebook: @LastKino</li>
+                                <li>Instagram: @lastkino_official</li>
+                                <li>TikTok: @lastkino</li>
+                                <li>YouTube: Last Kino</li>
+                                <li>Newsletter: zapisz się</li>
+                            </ul>
+
+                        </div>
+                    </div>
+            </div>
         </footer>
     )
 
 }
 
 export default Footer
+
