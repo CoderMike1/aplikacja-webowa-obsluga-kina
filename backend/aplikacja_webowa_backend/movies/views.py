cf1a2329ca8d14071a9540742e48159cd4d18860
hubercikk47 <hubert.niespial@onet.pl>
2025-11-04 14:41:19 +0100

    Stworzono endpoint to wyswietlania filmow po id

--- backend/aplikacja_webowa_backend/movies/views.py
+++ backend/aplikacja_webowa_backend/movies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Movie
@@ -16,3 +17,11 @@ class MovieByCategoryAPIView(APIView):
 
         return Response(data)
 
+class MovieDetailAPIView(APIView):
+    def get(self, request, movie_id):
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response({"detail": "Movie not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
