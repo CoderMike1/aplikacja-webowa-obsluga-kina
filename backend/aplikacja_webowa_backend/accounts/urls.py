ed9f4668fc5563d1c7ea740bc599b26a2ea1a25d
hubercikk47 <hubert.niespial@onet.pl>
2025-11-04 14:00:04 +0100

    Naprawa błędów związanych z refresh token

--- backend/aplikacja_webowa_backend/accounts/urls.py
+++ backend/aplikacja_webowa_backend/accounts/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from accounts import views
 from rest_framework_simplejwt.views import TokenRefreshView
-from accounts.views import CustomTokenObtainPairView
+from accounts.views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    RefreshTokenView,
+    UserInfo,
+    CustomTokenObtainPairView,
+)
 from .views import UserInfo
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
     path('me/',UserInfo.as_view(),name='user_info')
 ]
--- backend/aplikacja_webowa_backend/movies/models.py
+++ backend/aplikacja_webowa_backend/movies/models.py
@@ -1,36 +1,41 @@
-
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
 
-"""Movies model"""
+
 class Genre(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     original_title = models.CharField(max_length=255, blank=True)
     description = models.TextField(max_length=500, blank=True)
     release_date = models.DateField()
-    duration_minutes = models.PositiveIntegerField(help_text="Czas trwania w minutach")
+    duration_minutes = models.PositiveIntegerField(help_text="Runtime in minutes")
     genres = models.ManyToManyField(Genre, related_name='movies')
     directors = models.CharField(max_length=255, blank=True)
     poster_path = models.CharField(blank=True)
+    is_special_event = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def category(self):
         today = timezone.now().date()
+
+        if self.is_special_event:
+            return "special_event"
+
         if self.release_date <= today <= self.release_date + timedelta(days=30):
-            return "grane teraz"  # last 30 days
+            return "now_playing"
         elif self.release_date > today:
-            return "wkrótce"
+            return "upcoming"
         else:
-            return "wydarzenie specjalne"
+            return "classic"
 
     class Meta:
         ordering = ['-release_date', 'title']
