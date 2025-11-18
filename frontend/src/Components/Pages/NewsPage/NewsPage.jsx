dc14ea05dd268cf8cf151ffa9470c97962c3030c
Michal Ogorek <michal.ogorek03@gmail.com>
2025-11-18 13:49:21 +0100

    rozpoczeto budowe strony nowosci

--- frontend/src/Components/Pages/NewsPage/NewsPage.jsx
+++ frontend/src/Components/Pages/NewsPage/NewsPage.jsx
@@ -1,13 +1,78 @@
-
+import './NewsPage.css'
 
 const NewsPage = () =>{
 
     return (
-        <>
-        tutaj nowosci
-        </>
+        <div className="news__container">
+
+            <div className="news__container__inner">
+
+                <div className="news__item_playing">
+                    <div className="news__item_info">
+                        <h3>JUŻ TO GRAMY!</h3>
+                        <p>Kup bilet już teraz!</p>
+                    </div>
+
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+                <div className="news__item_playing">
+                    <div className="news__item_info">
+                        <h3>JUŻ TO GRAMY!</h3>
+                        <p>Kup bilet już teraz!</p>
+                    </div>
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+                <div className="news__item_playing">
+                    <div className="news__item_info">
+                        <h3>JUŻ TO GRAMY!</h3>
+                        <p>Kup bilet już teraz!</p>
+                    </div>
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+                <div className="news__item_playing">
+                    <div className="news__item_info">
+                        <h3>JUŻ TO GRAMY!</h3>
+                        <p>Kup bilet już teraz!</p>
+                    </div>
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+
+                <div className="news__item_coming_soon">
+                    <h3>Wkrótce!</h3>
+                    <p>Premiera: 19.11.2025</p>
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+                <div className="news__item_coming_soon">
+                    <h3>Wkrótce!</h3>
+                    <p>Premiera: 19.11.2025</p>
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+                <div className="news__item_coming_soon">
+                    <h3>Wkrótce!</h3>
+                    <p>Premiera: 19.11.2025</p>
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+                <div className="news__item_coming_soon">
+                    <h3>Wkrótce!</h3>
+                    <p>Premiera: 19.11.2025</p>
+                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
+                    <span>Świat lodu</span>
+                </div>
+
+            </div>
+
+
+        </div>
 
     )
 }
 
 export default NewsPage
+
