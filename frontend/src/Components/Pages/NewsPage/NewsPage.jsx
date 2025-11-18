53c9bcc82cdacc844fcff5768721de53bd65cb41
Michal Ogorek <michal.ogorek03@gmail.com>
2025-11-18 16:31:47 +0100

    ukonczono strone Nowosci

--- frontend/src/Components/Pages/NewsPage/NewsPage.jsx
+++ frontend/src/Components/Pages/NewsPage/NewsPage.jsx
@@ -1,72 +1,99 @@
 import './NewsPage.css'
+import {useEffect, useState} from "react";
+import {getMovies} from "../../../services/movieService.js";
+import {Link} from "react-router-dom";
 
 const NewsPage = () =>{
+    const [loading,setLoading] = useState(false)
+    const [latestMovies,setLatestMovies] = useState([])
+    const [upcomingMovies,setUpcomingMovies] = useState([])
+
+    const CACHE_KEY = "moviesCache";
+    // const CACHE_TTL_MS = 30 * 60 * 1000;
+    const CACHE_TTL_MS = 100;
+    useEffect(() => {
+        const loadMovies = async () => {
+            const cached = localStorage.getItem(CACHE_KEY);
+
+            if (cached) {
+                const parsed = JSON.parse(cached);
+                const isExpired = Date.now() - parsed.timestamp > CACHE_TTL_MS;
+
+                if (!isExpired) {
+                    setLatestMovies(parsed.nowPlaying);
+                    setUpcomingMovies(parsed.upcoming);
+                    return;
+                }
+            }
+
+            const resp = await getMovies();
+            const data = resp.data;
+            let twoWeeksAgo = new Date()
+            twoWeeksAgo.setDate(twoWeeksAgo.getDate()-1*15)
+            twoWeeksAgo = twoWeeksAgo.toISOString().slice(0,10)
+            const latest_movies = data.now_playing.filter(movie=>{
+                return movie.release_date < twoWeeksAgo;
+            })
+
+
+
+            setLatestMovies(latest_movies);
+            setUpcomingMovies(data.upcoming);
+            setLoading(false);
+
+            localStorage.setItem(
+                CACHE_KEY,
+                JSON.stringify({
+                    timestamp: Date.now(),
+                    nowPlaying: data.now_playing,
+                    upcoming: data.upcoming,
+                })
+            );
+        };
+
+        loadMovies();
+    }, []);
+
 
     return (
         <div className="news__container">
+            {
+                latestMovies.length > 0 || upcomingMovies.length > 0 ?
+                    <div className="news__container__inner">
+                        {
+                            latestMovies.map((m)=>(
+                                <div className="news__item_playing" key={m.id}>
+                                    <div className="news__item_info">
+                                        <h3>JUŻ TO GRAMY!</h3>
+                                    </div>
+                                    <img src={m.poster_path}/>
+                                    <span>{m.title}</span>
+                                    <Link  to={`/filmy/${m.id}`} className="news__item_link">
+                                        <h4>Kup Bilet!</h4>
+                                    </Link>
 
-            <div className="news__container__inner">
+                                </div>
+                            ))
+                        }
+                        {
+                            upcomingMovies.map((m)=>(
+                                <div className="news__item_playing" key={m.id}>
+                                    <div className="news__item_upcoming_info">
+                                        <h3>Wkrótce!</h3>
+                                        <p>Premiera: {m.release_date}</p>
+                                    </div>
+                                    <img src={m.poster_path}/>
+                                    <span>{m.title}</span>
+                                    <h4>Kup Bilet!</h4>
+                                </div>
+                            ))
+                        }
 
-                <div className="news__item_playing">
-                    <div className="news__item_info">
-                        <h3>JUŻ TO GRAMY!</h3>
-                        <p>Kup bilet już teraz!</p>
                     </div>
+                    :
+                    <p className="main_page_loading">Ładowanie danych...</p>
+            }
 
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-                <div className="news__item_playing">
-                    <div className="news__item_info">
-                        <h3>JUŻ TO GRAMY!</h3>
-                        <p>Kup bilet już teraz!</p>
-                    </div>
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-                <div className="news__item_playing">
-                    <div className="news__item_info">
-                        <h3>JUŻ TO GRAMY!</h3>
-                        <p>Kup bilet już teraz!</p>
-                    </div>
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-                <div className="news__item_playing">
-                    <div className="news__item_info">
-                        <h3>JUŻ TO GRAMY!</h3>
-                        <p>Kup bilet już teraz!</p>
-                    </div>
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-
-                <div className="news__item_coming_soon">
-                    <h3>Wkrótce!</h3>
-                    <p>Premiera: 19.11.2025</p>
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-                <div className="news__item_coming_soon">
-                    <h3>Wkrótce!</h3>
-                    <p>Premiera: 19.11.2025</p>
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-                <div className="news__item_coming_soon">
-                    <h3>Wkrótce!</h3>
-                    <p>Premiera: 19.11.2025</p>
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-                <div className="news__item_coming_soon">
-                    <h3>Wkrótce!</h3>
-                    <p>Premiera: 19.11.2025</p>
-                    <img src="https://image.tmdb.org/t/p/w500/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg"/>
-                    <span>Świat lodu</span>
-                </div>
-
-            </div>
 
 
         </div>
