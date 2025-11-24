9072e262f3dd9bbc930d22cbcda9e659e09eaa6c
Michal Ogorek <michal.ogorek03@gmail.com>
2025-11-24 21:34:49 +0100

    poprawiono logike wybierania rodzaju biletow

--- frontend/src/Components/Pages/CheckoutPages/SelectTickets/SelectTickets.jsx
+++ frontend/src/Components/Pages/CheckoutPages/SelectTickets/SelectTickets.jsx
@@ -6,27 +6,48 @@ const PRICES = {
 }
 
 const SelectTickets = ({checkout_data,setTickets,setStep}) =>{
-    console.log(checkout_data)
     return (
 
        <div className="checkout__tickets_container">
             <div className="checkout__tickets_list">
-                {checkout_data.seats.map((seat,i)=>(
-                    <div className="checkout__tickets_item" key={i}>
-                        <div className="ticket_info">
-                            <h3>Bilet #{i+1}</h3>
-                            <p>Rząd {seat.split("-")[0]} Miejsce {seat.split("-")[1]}</p>
-                        </div>
-                        <div className="ticket_option">
-                            <select className="ticket_option-select" onChange={(e)=>setTickets(e.target.value, seat,i,PRICES[e.target.value])}>
-                                <option value="">Wybierz opcję</option>
-                                <option value="ulgowy">Ulgowy 20zł</option>
-                                <option value="normalny">Normalny 30zł</option>
-                            </select>
-                        </div>
 
-                    </div>
-                ))}
+
+                {checkout_data.seats.map((seat,i)=>{
+                    const existingTicket = checkout_data.tickets?.find(t=>t.seat === seat);
+                    const selectedType = existingTicket?.ticketType || '';
+
+                    return (
+                        <div className="checkout__tickets_item" key={i}>
+                            <div className="ticket_info">
+                                <h3>Bilet #{i + 1}</h3>
+                                <p>Rząd {seat.split("-")[0]} Miejsce {seat.split("-")[1]}</p>
+                            </div>
+                            <div className="ticket_option">
+                                <select
+                                    className="ticket_option-select"
+                                    value={selectedType}
+                                    onChange={(e) =>
+                                        setTickets(
+                                            e.target.value,
+                                            seat,
+                                            i,
+                                            PRICES[e.target.value]
+                                        )
+                                    }
+                                >
+                                    {!selectedType && (
+                                        <option value="">
+                                            Wybierz opcję
+                                        </option>
+                                    )}
+                                    <option value="ulgowy">Ulgowy 20zł</option>
+                                    <option value="normalny">Normalny 30zł</option>
+                                </select>
+                            </div>
+                        </div>
+                    );
+                })}
+
             </div>
            <div className="checkout_s1_submit">
                <button
